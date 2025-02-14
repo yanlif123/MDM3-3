8d9b401f024178364d7a2fdd0efe7964a207b55d
Finlay Michael <finlaymichael123@gmail.com>
2025-02-14 15:41:18 +0000

    test

---
+++ .spyder-py3/history.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Sep 23 14:58:41 2024)---
+import sys
+print(sys.exe)
+cutable
+print(sys.executable)
+
+## ---(Mon Sep 23 16:01:37 2024)---
+cd
+cd PyBristolFE/src
+cd test
+cd /test
+ls
+runfile('C:/Users/User/PyBristolFE/src/PyBristolFE.py', args='job = ../test/Bridge.inp', wdir='C:/Users/User/PyBristolFE/src')
+
+## ---(Wed Sep 25 17:42:40 2024)---
+runfile('C:/Users/User/PyBristolFE/src/PyBristolFE.py', args='job = ../test/Bridge.inp', wdir='C:/Users/User/PyBristolFE/src')
+runfile('C:/Users/User/CW3/CW3_folder/CW3.py', wdir='C:/Users/User/CW3/CW3_folder')
